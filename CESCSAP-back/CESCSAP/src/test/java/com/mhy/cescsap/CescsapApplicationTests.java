5f715f248dd31442e47ea81acac3754061251aa2
尘心_无愿 <2568237200@qq.com>
2024-12-09 10:45:34 +0800

    初始化后端

---
+++ CESCSAP-back/CESCSAP/src/test/java/com/mhy/cescsap/CescsapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhy.cescsap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CescsapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
