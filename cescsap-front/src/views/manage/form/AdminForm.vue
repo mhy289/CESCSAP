14a9c0dff254fec84a51106009db7e0191b7db32
尘心_无愿 <2568237200@qq.com>
2025-02-09 21:09:47 +0800

    学生端交互2.5.2

---
+++ cescsap-front/src/views/manage/form/AdminForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-form>
+    <UserForm></UserForm>
+
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
+
+</template>
+
+<script>
+  import UserForm from './Form.vue'
+
+  export default ({
+    data() {
+      return {
+
+      }
+    },
+    components: {
+      UserForm,
+    },
+    methods: {
+      onSubmit() {
+        console.log(this.name);
+      }
+    }
+  })
+
+</script>
---
+++ cescsap-front/src/views/manage/form/Form.vue
@@ -0,0 +1,48 @@
+<template>
+  <el-form ref="form" :model="form" label-width="80px">
+    <el-form-item label="id">
+      <el-input v-model="form.id"></el-input>
+    </el-form-item>
+    <el-form-item label="名称">
+      <el-input v-model="form.name"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input v-model="form.password"></el-input>
+    </el-form-item>
+    <el-form-item label="联系方式">
+      <el-input type="textarea" v-model="form.contact"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">立即创建</el-button>
+      <el-button>取消</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        form: {
+          id: '',
+          password: '',
+          contact: '',
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        }
+      }
+    },
+    methods: {
+      onSubmit() {
+        this.$emit('submit', this.name);
+      }
+    }
+  }
+
+</script>
